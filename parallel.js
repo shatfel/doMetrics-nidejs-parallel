fe2a60c76c6701e8916362b60d22848871b11a18
shatfel@dell <shatfel@gmail.com>
2021-03-28 01:35:56 +0300

    fix and format code

--- parallel.js
+++ parallel.js
@@ -2,22 +2,34 @@
 
 const { spawn } = require('child_process');
 
+// metrics
 let _params = [ 'freemem', 'loadavg_1', 'loadavg_5', 'loadabg_15' ]
+// parallel sessions
 let doMetrics = []
 
+// show metric which we check
 for (let i in _params) {console.log('/i/ add metric for check: ' +_params[i])}
 
+// for each metric
 for (let _p in _params) {
   let pp=_params[_p]
   
+	// run parallel process
   console.log('/i/ running process for metric: ' +pp)
   doMetrics[pp] = spawn('./doMetrics', [pp]);
   
-doMetrics[pp].stdout.on('data', (data) => {
-	  console.log(pp+' stdout: ' + data);
-	  })
+	// stdout for each process
+	doMetrics[pp].stdout.on('data', (data) => {
+		console.log(pp+' stdout: ' + data);
+		})
 
-	  	  doMetrics[pp].on('close', (code) => {
-	console.log(pp + ': child process exited with code: '+ code);
-	  	  	  })
+	// stderr for each process
+	doMetrics[pp].stderr.on('data', (data) => {
+		console.log(pp+' stderr: ' + data);
+		})
+
+	// when process finished
+	doMetrics[pp].on('close', (code) => {
+		console.log(pp + ': child process exited with code: '+ code);
+		})
 }
